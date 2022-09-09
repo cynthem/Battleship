163411c386066c209f21e66a48b0d82cd203cf5b
doozles411 <cindy.phelps01@gmail.com>
2022-09-09 12:29:36 -0700

    Add start button DOM changing logic

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ const startBtn = document.querySelector('.welcome-btn');
 const playerName = document.querySelector('.player-name');
 const compName = document.querySelector('.computer-name');
 const welcomeMsg = document.querySelector('.welcome');
-const 
+const gameText = document.querySelector('.gameplay-text');
 
 startBtn.addEventListener('click', (e) => {
     e.preventDefault();
     playerName.textContent = startInput.value;
     playerName.classList.remove('invisible'); 
     compName.classList.remove('invisible'); 
+    welcomeMsg.classList.add('hide');
+    playerName.value = '';
+    gameText.classList.remove('hide');
 });
 
 
