afe4b34e36b3e53f034d589f54cf169382ca4793
doozles411 <cindy.phelps01@gmail.com>
2022-09-09 13:03:53 -0700

    Begin logic for rendering game text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// GAME START
 const startInput = document.querySelector('.welcome-input');
 const startBtn = document.querySelector('.welcome-btn');
 const playerName = document.querySelector('.player-name');
@@ -16,3 +17,25 @@ startBtn.addEventListener('click', (e) => {
 });
 
 
+// GAME PLAY
+
+const gameBoard = document.querySelectorAll('.right-board > button');
+const textStart = 'Make your move . . .';
+const textPlayer = 'You fire a shot into enemy waters . . .';
+const textComputer = 'The enemy fires a shot into your waters . . .';
+const textCompTurn = 'The enemy is taking aim . . .';
+const textMiss = 'and it\'s a miss.';
+const textHit = 'and it\'s a hit!';
+const textSunkPlayer = 'You\'ve sunk their battleship.';
+const textSunkComp = 'They\'ve sunk your battleship.';
+const textWin = 'Congratulations, name. You\'re the winner!';
+const textLose = 'The enemy has won. Better luck next time.';
+
+gameBoard.forEach(cell => {
+    cell.addEventListener('click', () => {
+        gameText.textContent = textPlayer;
+    });
+});
+
+
+// GAME END
