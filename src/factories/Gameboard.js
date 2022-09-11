fc9420fc61ed88a2baa763c094b2980b321b7b0a
doozles411 <cindy.phelps01@gmail.com>
2022-09-11 12:29:07 -0700

    Remove unnecessary returns

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -30,31 +30,26 @@ class Gameboard {
             const starting1 = arr1[Math.floor(Math.random() * arr1.length)];
             const carrierLocation = [starting1, starting1 + 1, starting1 + 2, starting1 + 3, starting1 + 4];
             carrierLocation.forEach(cell => this.board[cell].shipId = 'carrier');
-            return carrierLocation;
         } else if (shipName === this.battleship) {
             const arr2 = [8, 9, 18, 19, 28, 29, 38, 39, 48, 49, 58, 59, 68, 69];
             const starting2 = arr2[Math.floor(Math.random() * arr2.length)];
             const battleLocation = [starting2, starting2 + 10, starting2 + 20, starting2 + 30];
             battleLocation.forEach(cell => this.board[cell].shipId = 'battleship');
-            return battleLocation;
         } else if (shipName === this.cruiser) {
             const arr3 = [42, 43, 44, 52, 53, 54, 62, 63, 64];
             const starting3 = arr3[Math.floor(Math.random() * arr3.length)];
             const cruiserLocation = [starting3, starting3 + 1, starting3 + 2];
             cruiserLocation.forEach(cell => this.board[cell].shipId = 'cruiser');
-            return cruiserLocation;
         } else if (shipName === this.submarine) {
             const arr4 = [0, 10, 20, 30, 40, 50, 60, 70];
             const starting4 = arr4[Math.floor(Math.random() * arr4.length)];
             const subLocation = [starting4, starting4 + 10, starting4 + 20];
             subLocation.forEach(cell => this.board[cell].shipId = 'submarine');
-            return subLocation;
         } else if (shipName === this.destroyer) {
             const arr5 = [82, 83, 84, 85, 92, 93, 94, 95];
             const starting5 = arr5[Math.floor(Math.random() * arr5.length)];
             const destroyLocation = [starting5, starting5 + 1];
             destroyLocation.forEach(cell => this.board[cell].shipId = 'destroyer');
-            return destroyLocation;
         }
     }
 
@@ -64,6 +59,7 @@ class Gameboard {
         
         if (status.shipId === 'none') {
             return status;
+
         } else if (status.shipId === 'carrier') {
             this.carrier.isHit(index);
             if (!this.carrier.isSunk()) {
@@ -78,6 +74,7 @@ class Gameboard {
                     return status;
                 }
             }
+
         } else if (status.shipId === 'battleship') {
             this.carrier.isHit(index);
             if (!this.carrier.isSunk()) {
@@ -92,6 +89,7 @@ class Gameboard {
                     return status;
                 }
             }
+
         } else if (status.shipId === 'cruiser') {
             this.carrier.isHit(index);
             if (!this.carrier.isSunk()) {
@@ -106,6 +104,7 @@ class Gameboard {
                     return status;
                 }
             }
+
         } else if (status.shipId === 'submarine') {
             this.carrier.isHit(index);
             if (!this.carrier.isSunk()) {
@@ -120,6 +119,7 @@ class Gameboard {
                     return status;
                 }
             }
+            
         } else if (status.shipId === 'destroyer') {
             this.carrier.isHit(index);
             if (!this.carrier.isSunk()) {
